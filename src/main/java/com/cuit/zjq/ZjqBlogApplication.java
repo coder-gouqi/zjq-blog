327a7b7e917cb245e1446447d604cc84f29cf177
zhJQ1218 <977274456@qq.com>
2023-11-02 16:59:43 +0800

    博客系统项目初始化

---
+++ src/main/java/com/cuit/zjq/ZjqBlogApplication.java
@@ -0,0 +1,13 @@
+package com.cuit.zjq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjqBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjqBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuit/zjq/ZjqBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuit.zjq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjqBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
