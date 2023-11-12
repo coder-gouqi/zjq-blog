4d01ec548cb273754f6a8756aee2f39ae96f06c0
zhJQ1218 <977274456@qq.com>
2023-11-12 11:48:14 +0800

    增加更新文章点赞数定时任务

--- src/main/java/com/cuit/zjq/job/ThumbJob.java
+++ src/main/java/com/cuit/zjq/job/ThumbJob.java
@@ -1,8 +1,18 @@
 package com.cuit.zjq.job;
 
+import com.cuit.zjq.common.InteractionRequest;
+import com.cuit.zjq.mapper.CommentMapper;
+import com.cuit.zjq.mapper.EssayMapper;
+import com.cuit.zjq.mapper.ThumbMapper;
+import com.cuit.zjq.model.domain.Essay;
+import com.cuit.zjq.model.domain.Thumb;
+import com.cuit.zjq.model.dto.essay.EssayQueryRequest;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 点赞定时任务
@@ -10,9 +20,35 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class ThumbJob {
-    //每十分钟执行同步点赞数
-    @Scheduled(cron = "0 */10 * * * ?")
-    public void synchronizeThumb() {
 
+    @Resource
+    private EssayMapper essayMapper;
+
+    @Resource
+    private CommentMapper commentMapper;
+
+    @Resource
+    private ThumbMapper thumbMapper;
+
+    //每十分钟执行同步点赞数
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void synchronizeThumb() {
+        //查询所有文章
+        EssayQueryRequest essayQueryRequest = new EssayQueryRequest();
+        List<Essay> essayList = essayMapper.select(essayQueryRequest);
+        //遍历所有id
+        for (Essay essay : essayList) {
+            String id = essay.getId();
+            InteractionRequest interactionRequest = new InteractionRequest();
+            interactionRequest.setEssayId(id);
+            List<Thumb> thumbList = thumbMapper.select(interactionRequest);
+            int thumbCount = thumbList.size();
+            Essay essayForUpdate = essayMapper.selectById(id);
+            essayForUpdate.setThumbNum(thumbCount);
+            int updateById = essayMapper.updateById(essayForUpdate);
+            if (updateById > 0) {
+                System.out.println("更新文章" + id + "点赞数成功");
+            }
+        }
     }
 }
--- src/main/java/com/cuit/zjq/mapper/ThumbMapper.java
+++ src/main/java/com/cuit/zjq/mapper/ThumbMapper.java
@@ -1,7 +1,12 @@
 package com.cuit.zjq.mapper;
 
 import com.cuit.zjq.common.InteractionRequest;
+import com.cuit.zjq.model.domain.Essay;
+import com.cuit.zjq.model.domain.Thumb;
+import com.cuit.zjq.model.dto.essay.EssayQueryRequest;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * @description 针对表【thumb】的数据库操作Mapper
@@ -10,6 +15,8 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ThumbMapper {
 
     int insert(InteractionRequest interactionRequest);
+
+    List<Thumb> select(InteractionRequest interactionRequest);
 
     int delete(InteractionRequest interactionRequest);
 }
